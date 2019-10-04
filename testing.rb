f1019022c5883c28b4e17cc65b6b870b5b426e21
Joao Nascimento <joao.nascimento@acad.pucrs.br>
2019-10-04 18:03:36 -0300

    adjust

--- testing.rb
+++ testing.rb
@@ -2,9 +2,10 @@ require 'watir';
 browser = Watir::Browser.start 'https://docs.google.com/forms/d/e/1FAIpQLSdZaaC5z-39_b0gi1kjX4GvBeWgybs2HfIyPxCe52hcZtznNQ/viewform?usp=sf_link'
 browser.input(jsname: 'YPqjbf').send_keys('Joao Nascimento')
 browser.input(type: 'email').send_keys('joao@mail.com')
-for i in (0..1000).step(5)
+for i in (0..40).step(5)
     browser.driver.execute_script("window.scrollBy(0,#{i})")
     sleep 0.1
 end
+#browser.div(jsname: 'W85ice').send_keys('PUCRS')
 
-sleep 5
+sleep 10
