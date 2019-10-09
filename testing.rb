e7f6c160aea1a1b613a544d2bf2b7bf1db66d2ac
Joao Nascimento <joao.nascimento@acad.pucrs.br>
2019-10-08 21:02:25 -0300

    aaa

--- testing.rb
+++ testing.rb
@@ -1,11 +1,21 @@
 require 'watir';
-browser = Watir::Browser.start 'https://docs.google.com/forms/d/e/1FAIpQLSdZaaC5z-39_b0gi1kjX4GvBeWgybs2HfIyPxCe52hcZtznNQ/viewform?usp=sf_link'
-browser.input(jsname: 'YPqjbf').send_keys('Joao Nascimento')
-browser.input(type: 'email').send_keys('joao@mail.com')
-for i in (0..40).step(5)
-    browser.driver.execute_script("window.scrollBy(0,#{i})")
-    sleep 0.1
-end
+browser = Watir::Browser.start 'http://localhost:8080'
+browser.a(class: 'btn create').click
+sleep 2
+browser.input(class: 'email').send_keys('pompermaier@mail.com')
+sleep 2
+browser.input(class: 'nick').send_keys('pomper_veranopolis')
+sleep 2
+browser.input(class: 'categorias').send_keys :backspace
+browser.input(class: 'categorias').send_keys '6'
+sleep 2
+browser.input(class: 'players').set('')
+browser.input(class: 'players').send_keys('4')
+#browser.input(type: 'email').send_keys('joao@mail.com')
+#for i in (0..40).step(5)
+#    browser.driver.execute_script("window.scrollBy(0,#{i})")
+#    sleep 0.1
+#end
 #browser.div(jsname: 'W85ice').send_keys('PUCRS')
 # input > find by name
-sleep 10
+sleep 3
