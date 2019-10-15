4d75eb504b9bd5693b602bf894fb2292a18287e5
mrrodrigo <r.mello@edu.pucrs.br>
2019-10-15 15:22:51 -0300

    Go :horse:

--- testing.rb
+++ testing.rb
@@ -2,21 +2,72 @@ require 'watir';
 
 browser = Watir::Browser.start 'http://localhost:8080'
 
-sizing = true
-while sizing
-  browser.window.resize_to(684, 768)
-  browser.window.move_to(1, 1)
-  sizing = false
-end
+criar_jogo = Thread.new {
+  sizing = true
+  while sizing
+    browser.window.resize_to(684, 768)
+    browser.window.move_to(1, 1)
+    sizing = false
+  end
+  # create game
+  browser.a(class: 'btn create').click
+  sleep 2
+  #### check email e nick
+  browser.input(class: 'email').send_keys('pomp.com')
+  sleep 3
+  browser.input(class: 'nick').send_keys('p')
 
+  invalid_msg_create_game = browser.p(class: 'invalidEmailMsg').text
 
-Thread.new {
-  browser2 = Watir::Browser.start 'http://localhost:8080'
+  if invalid_msg_create_game == 'Email inválido'
+    puts "Teste com email inválido passou. Na criação da sala de jogo"
+  end
 
+  invalid_nick_msg_create_game = browser.p(class: 'invalidNickMsg').text
+
+  if invalid_nick_msg_create_game == 'Apelido inválido'
+    puts "Teste com nick inválido passou. Na criação da sala de jogo"
+  end
+
+  ######
+  sleep 4
+  browser.input(class: 'email').to_subtype.clear
+  browser.input(class: 'nick').to_subtype.clear
+  browser.input(class: 'email').send_keys('pompermaier@mail.com')
+  sleep 3
+  browser.input(class: 'nick').send_keys('pomper_tester')
+  invalid_msg_create_game = browser.p(class: 'invalidEmailMsg').text
+
+  if invalid_msg_create_game != 'Email inválido'
+    puts "Teste com email válido passou. Na criação da sala de jogo"
+  end
+
+  invalid_nick_msg_create_game = browser.p(class: 'invalidNickMsg').text
+
+  if invalid_nick_msg_create_game != 'Apelido inválido'
+    puts "Teste com nick válido passou. Na criação da sala de jogo"
+  end
+
+  sleep 4
+  browser.input(class: 'categorias').send_keys :backspace
+  browser.input(class: 'categorias').send_keys '6'
+  sleep 2
+  browser.input(class: 'players').send_keys :backspace
+  browser.input(class: 'players').send_keys '4'
+  browser.a(class: 'btn create').click
+  sleep 1
+
+}
+
+criar_jogo.join
+
+browser2 = Watir::Browser.start 'http://localhost:8080'
+
+entrar_jogo = Thread.new {
   sizing = true
   while sizing
     browser2.window.resize_to(684, 768)
-    browser2.window.move_to(684 , 1 )
+    browser2.window.move_to(684 , 1)
     sizing = false
   end
   
@@ -27,6 +78,19 @@ Thread.new {
   browser2.input(class: 'email').send_keys('pomp.com')
   sleep 3
   browser2.input(class: 'nick').send_keys('p')
+
+  invalid_email_msg_join_game = browser2.p(class: 'invalidEmailMsg').text
+
+  if invalid_email_msg_join_game == 'Email inválido'
+    puts "Teste com email inválido passou. Entrar na sala de jogo"
+  end
+
+  invalid_nick_msg_join_game = browser2.p(class: 'invalidNickMsg').text
+
+  if invalid_nick_msg_join_game == 'Apelido inválido'
+    puts "Teste com nick inválido passou. Entrar na sala de jogo"
+  end
+
   ###
   sleep 5
   ####  email e nick certo
@@ -35,38 +99,30 @@ Thread.new {
   browser2.input(class: 'nick').to_subtype.clear
   sleep 3
   browser2.input(class: 'email').send_keys('pompermaier@mail.com')
+  sleep 3
   browser2.input(class: 'nick').send_keys('pompermaier_veranopolis')
+
+  invalid_email_msg_join_game = browser.p(class: 'invalidEmailMsg').text
+
+  if invalid_email_msg_join_game != 'Email inválido'
+    puts "Teste com email válido passou. Entrar na sala de jogo"
+  end
+
+  invalid_nick_msg_join_game = browser.p(class: 'invalidNickMsg').text
+
+  if invalid_nick_msg_join_game != 'Apelido inválido'
+    puts "Teste com nick válido passou. Entrar na sala de jogo"
+  end
+
   #######
   sleep 3
   browser2.input(class: 'id').send_keys :backspace
-  browser2.input(class: 'id').send_keys('10032')
+  browser2.input(class: 'id').send_keys('10041')
   sleep 1
   browser2.a(class: 'btn join').click
 }
 
-
-sleep 12
-# create game
-browser.a(class: 'btn create').click
-sleep 2
-#### check email e nick
-browser.input(class: 'email').send_keys('pomp.com')
-sleep 3
-browser.input(class: 'nick').send_keys('p')
-######
-sleep 4
-browser.input(class: 'email').to_subtype.clear
-browser.input(class: 'nick').to_subtype.clear
-browser.input(class: 'email').send_keys('pompermaier@mail.com')
-browser.input(class: 'nick').send_keys('pomper_tester')
-sleep 4
-browser.input(class: 'categorias').send_keys :backspace
-browser.input(class: 'categorias').send_keys '6'
-sleep 2
-browser.input(class: 'players').send_keys :backspace
-browser.input(class: 'players').send_keys '4'
-browser.a(class: 'btn create').click
-sleep 1
+entrar_jogo.join
 
 # espera jogador 2 entrar
 waiting_player = true
@@ -80,7 +136,6 @@ while waiting_player
     waiting_player = false
   end
 end
-
 browser.a(class: 'btn init').click
 
 sleep 100
